0ece85add7e04b3a39a31dd81d5e2ba10e6de899
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-23 13:48:59 -0300

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Function that concat two strings str1 and str2 and return a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function
+
+    Args:
+        str1 (str): string 1
+        str2 (str): string 2
+
+    Return:
+        str: str1 + str2
+    """
+    return str1 + str2
