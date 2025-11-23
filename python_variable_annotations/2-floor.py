ce666bbf81ef8405d0c62308b7b97bde5fa34c95
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-23 14:48:39 -0300

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Function that gets a float and return its floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function
+
+    Args:
+        n (float): n number
+
+    Return:
+        int: n float
+    """
+    return int(n)
