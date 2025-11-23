9814e6fd53cf8d7c5849d7434c991f3427ad3b87
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-23 14:52:24 -0300

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Function that gets a float and return it in a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function
+
+    Args:
+        n (float): n number
+
+    Return:
+        str: n string 
+    """
+    return str(n)
